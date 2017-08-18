1f874c644c420a8d9e685a6b8d4d76b29344516a
yuye <yuye@youjuke.com>
2017-08-18 11:16:30 +0800

    new s

--- MessageUI/ViewController.m
+++ MessageUI/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <MessageUI/MessageUI.h>
+@interface ViewController ()<MFMessageComposeViewControllerDelegate>
 
 @end
 
@@ -16,8 +16,74 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *messagebtn = [[UIButton alloc] init];
+    messagebtn.translatesAutoresizingMaskIntoConstraints = NO;
+    messagebtn.backgroundColor = [UIColor redColor];
+    [messagebtn setTitle:@"发短信" forState:UIControlStateNormal];
+    [messagebtn setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+    [messagebtn addTarget:self action:@selector(clickAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:messagebtn];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[messagebtn]-50-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(messagebtn)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[messagebtn(60)]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(messagebtn)]];
+
 }
+
+
+- (void)clickAction{
+    [self showMessageView:@[@"13503767802"] title:@"标题党" body:@"哈哈哈哈"];
+}
+
+
+#pragma mark - 发送短信方法
+-(void)showMessageView:(NSArray *)phones title:(NSString *)title body:(NSString *)body
+{
+    if( [MFMessageComposeViewController canSendText] )
+    {
+        MFMessageComposeViewController * controller = [[MFMessageComposeViewController alloc] init];
+        controller.recipients = phones;
+        controller.navigationBar.tintColor = [UIColor redColor];
+        controller.body = body;
+        controller.messageComposeDelegate = self;
+        [self presentViewController:controller animated:YES completion:nil];
+        [[[[controller viewControllers] lastObject] navigationItem] setTitle:title];//修改短信界面标题
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示信息"
+                                                        message:@"该设备不支持短信功能"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"确定"
+                                              otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+
+
+
+#pragma mark - 代理方法
+-(void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    switch (result) {
+        case MessageComposeResultSent:
+            //信息传送成功
+            
+            break;
+        case MessageComposeResultFailed:
+            //信息传送失败
+            
+            break;
+        case MessageComposeResultCancelled:
+            //信息被用户取消传送
+            
+            break;
+        default:
+            break;
+    }
+}
+
 
 
 - (void)didReceiveMemoryWarning {
